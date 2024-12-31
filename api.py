c0016b1d72c80785abf3b8b3a831736b07f2499e
Ricky Waldt <121530629+rickyharvywaldt@users.noreply.github.com>
2024-12-31 14:57:37 +0100

    add dumps and jq for cleaner terminal output

--- api.py
+++ api.py
@@ -1,12 +1,15 @@
 import requests as r
+import json
 
 response = r.get("https://api.data.gov.sg/v1/environment/air-temperature").json()
 
-for item in response:
+# for item in response:
 
-    stations = response["metadata"]["stations"]
+#     stations = response["metadata"]["stations"]
 
-    for station in stations:
+#     for station in stations:
 
-        print(f'De id van {station["name"]} = {station["id"]}')
+#         print(f'De id van {station["name"]} = {station["id"]}')
 
+# python3 api.py | jq (prettier output)
+print(json.dumps(response))
